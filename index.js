26c6d68dbaa7ef311a3c89234db77ff166cfd355
isaac <isaaccastrocisneros@gmail.com>
2022-10-26 09:54:22 -0500

    si

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'));
 
 app.get('/',(req,res)=>
 {
     res.json('qui estoy')
 })
+
+if(process.env.NODE_ENV!=='production')
+{
+    app.get('*',(req,res)=>
+    {
+        req.sendFile(path.resolve('dist','index.html'))
+    })
+}
+
 
 app.listen(process.env.PORT||3000,()=>
 {
